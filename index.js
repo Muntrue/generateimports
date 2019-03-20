f8e2b98fc8ca491f043aca32304a7e0cb02f9fbe
mario <mario.smit@mybit.nl>
2019-03-20 12:09:31 +0100

    Added function to create output folder if it does not already exist

--- index.js
+++ index.js
@@ -1,17 +1,20 @@
-const fs = require("fs");
+const fs   = require("fs");
 const glob = require("glob");
+const path = require("path");
+
 
 /**
  * Generates import file for scss
  *
  * @param {Object} input
  */
-function scss(input) {
+function scss(input)
+{
     checkParams(input);
 
     let imports = [];
 
-    if (input.hasOwnProperty("files")) imports = imports.concat(input.files);
+    if ( input.hasOwnProperty("files") ) imports = imports.concat(input.files);
     input.folders.forEach(folder => imports = imports.concat(getFileList(folder, "scss", input)));
 
     const formatter = file => "@import '" + file + "';";
@@ -23,16 +26,16 @@ function scss(input) {
  *
  * @param {Object} input
  */
-function vue(input) {
+function vue(input)
+{
     checkParams(input);
 
     let imports = [];
 
-    if (input.hasOwnProperty("files")) imports = imports.concat(input.files);
+    if ( input.hasOwnProperty("files") ) imports = imports.concat(input.files);
     input.folders.forEach(folder => imports = imports.concat(getFileList(folder, "vue", input)));
 
-    const formatter = file => "import " + toCamelCase(path.parse(file).name) + " from '" + file + "';\n" +
-    "Vue.component('" + path.parse(file).name + "', " + toCamelCase(path.parse(file).name) + ");\n";
+    const formatter = file => "import " + toCamelCase(path.parse(file).name) + " from '" + file + "';\n" + "Vue.component('" + path.parse(file).name + "', " + toCamelCase(path.parse(file).name) + ");\n";
     fs.writeFileSync(input.output, imports.map(formatter).join("\r\n"));
 }
 
@@ -41,12 +44,13 @@ function vue(input) {
  *
  * @param {Object} input
  */
-function js(input) {
+function js(input)
+{
     checkParams(input);
 
     let imports = [];
 
-    if (input.hasOwnProperty("files")) imports = imports.concat(input.files);
+    if ( input.hasOwnProperty("files") ) imports = imports.concat(input.files);
     input.folders.forEach(folder => imports = imports.concat(getFileList(folder, "js", input)));
 
     const formatter = file => "import " + toCamelCase(path.parse(file).name) + " from '" + file + "';" + "\n" + "global." + toCamelCase(path.parse(file).name) + " = " + toCamelCase(path.parse(file).name) + ";\n";
@@ -61,11 +65,14 @@ function js(input) {
  * @param {Object} input
  * @returns {Array}
  */
-function getFileList(folder, extension, input) {
+function getFileList(folder, extension, input)
+{
     const list = [];
 
-    glob.sync(folder).forEach(function (directory) {
-        glob.sync(directory + "/**/*." + extension).forEach(function (file) {
+    glob.sync(folder).forEach(function (directory)
+    {
+        glob.sync(directory + "/**/*." + extension).forEach(function (file)
+        {
             list.push(forwardSlash(path.relative(input.output, file).substr(3)));
         });
     });
@@ -79,8 +86,10 @@ function getFileList(folder, extension, input) {
  * @param {string} str
  * @returns {string}
  */
-function toCamelCase(str) {
-    return str.split(/[_-]/g).map(part => {
+function toCamelCase(str)
+{
+    return str.split(/[_-]/g).map(part =>
+    {
         return part.charAt(0).toUpperCase() + part.slice(1);
     }).join("");
 }
@@ -91,24 +100,43 @@ function toCamelCase(str) {
  * @param str
  * @returns {*}
  */
-function forwardSlash(str){
-    return str.replace(/\\/g,"/");
+function forwardSlash(str)
+{
+    return str.replace(/\\/g, "/");
 }
+
 /**
  * Checks for correct params
  *
  * @param {Object} input
  */
-function checkParams(input) {
-    if (!input.hasOwnProperty("folders")) throw new TypeError("folders propery is missing");
-    if (!input.hasOwnProperty("output")) throw new TypeError("output propery is missing");
+function checkParams(input)
+{
+    if ( !input.hasOwnProperty("folders") ) throw new TypeError("folders propery is missing");
+    if ( !input.hasOwnProperty("output") ) throw new TypeError("output propery is missing");
 
-    if (input.hasOwnProperty("files")) {
-        if (typeof input.files !== "object") throw new TypeError("files property must contain an array");
+    if ( input.hasOwnProperty("files") )
+    {
+        if ( typeof input.files !== "object" ) throw new TypeError("files property must contain an array");
     }
 
-    if (typeof input.folders !== "object") throw new TypeError("folders property must contain an array");
-    if (typeof input.output !== "string") throw new TypeError("input property must contain a string");
+    if ( typeof input.folders !== "object" ) throw new TypeError("folders property must contain an array");
+    if ( typeof input.output !== "string" ) throw new TypeError("input property must contain a string");
+
+    createDirIfNotExist(input.output);
+}
+
+/**
+ * Create output directory if it does not exist
+ *
+ * @param path
+ */
+function createDirIfNotExist(dirPath)
+{
+    if ( !fs.existsSync(path.dirname(dirPath)) )
+    {
+        fs.mkdirSync(path.dirname(dirPath), { recursive: true });
+    }
 }
 
 /**
